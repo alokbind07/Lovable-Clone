65c38dccacc6cdb70cd14e091b5b0e270b1db985
Alok Bind <alokbind4733@gmail.com>
2026-01-10 02:14:00 +0530

    Spring Security Added

---
+++ src/main/java/com/alokbind/projects/lovable_clone/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.alokbind.projects.lovable_clone.mapper;
+
+import com.alokbind.projects.lovable_clone.dto.auth.SignupRequest;
+import com.alokbind.projects.lovable_clone.dto.auth.UserProfileResponse;
+import com.alokbind.projects.lovable_clone.entity.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    User toEntity(SignupRequest signupRequest);
+
+    UserProfileResponse toUserProfileResponse(User user);
+}
