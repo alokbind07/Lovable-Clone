df483bdfa6118d62dfa8869f8ade2bff2d7b0b17
Alok Bind <alokbind4733@gmail.com>
2026-01-14 01:54:27 +0530

    Stripe Payments Integration

--- src/main/java/com/alokbind/projects/lovable_clone/service/impl/StripePaymentProcessor.java
+++ src/main/java/com/alokbind/projects/lovable_clone/service/impl/StripePaymentProcessor.java
@@ -3,13 +3,58 @@ package com.alokbind.projects.lovable_clone.service.impl;
 import com.alokbind.projects.lovable_clone.dto.subscription.CheckoutRequest;
 import com.alokbind.projects.lovable_clone.dto.subscription.CheckoutResponse;
 import com.alokbind.projects.lovable_clone.dto.subscription.PortalResponse;
+import com.alokbind.projects.lovable_clone.entity.Plan;
+import com.alokbind.projects.lovable_clone.error.ResourceNotFoundException;
+import com.alokbind.projects.lovable_clone.repository.PlanRepository;
+import com.alokbind.projects.lovable_clone.security.AuthUtil;
 import com.alokbind.projects.lovable_clone.service.PaymentProcessor;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class StripePaymentProcessor implements PaymentProcessor {
+
+    private final AuthUtil authUtil;
+    private final PlanRepository planRepository;
+
+    @Value("${client.url}")
+    private String frontendUrl;
 
     @Override
     public CheckoutResponse createCheckoutSessionUrl(CheckoutRequest request) {
-        return null;
+        Plan plan = planRepository.findById(request.planId()).orElseThrow(() ->
+                new ResourceNotFoundException("Plan", request.planId().toString()));
+
+        Long userId = authUtil.getCurrentUserId();
+
+        SessionCreateParams params = SessionCreateParams.builder()
+                .addLineItem(
+                        SessionCreateParams.LineItem.builder().setPrice(plan.getStripePriceId()).setQuantity(1L).build())
+                .setMode(SessionCreateParams.Mode.SUBSCRIPTION)
+                .setSubscriptionData(
+                        new SessionCreateParams.SubscriptionData.Builder()
+                                .setBillingMode(SessionCreateParams.SubscriptionData.BillingMode.builder()
+                                        .setType(SessionCreateParams.SubscriptionData.BillingMode.Type.FLEXIBLE)
+                                        .build())
+                                .build()
+                )
+                .setSuccessUrl(frontendUrl + "/success.html?session_id={CHECKOUT_SESSION_ID}")
+                .setCancelUrl(frontendUrl + "/cancel.html")
+                .putMetadata("user_id", userId.toString())
+                .putMetadata("plan_id", plan.getId().toString())
+                .build();
+
+        try {
+            Session session = Session.create(params);
+            return new CheckoutResponse(session.getUrl());
+        } catch (StripeException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
